e0196fd22db1d958ea0582d1bc4be38282d6f147
정재현 <chwogus0303@naver.com>
2024-11-22 19:33:12 +0900

    feat(chatbot-app): 챗봇 서버 구현

---
+++ chatbot-app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from core.docs import *
+from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+
+def get_server():
+    server = FastAPI(
+        title='Chatbot', 
+        docs_url="/docs", redoc_url=None,
+        version="1.0.0",
+        description=chatbot_description,
+        openapi_url="/openapi.json"
+    )
+    server.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+    server.add_middleware(
+        GZipMiddleware, minimum_size=1000
+    )
+
+    return server
+
+app = get_server()
+
+@app.get('/api/ping', tags=['Root'])
+def ping():
+    return 200
