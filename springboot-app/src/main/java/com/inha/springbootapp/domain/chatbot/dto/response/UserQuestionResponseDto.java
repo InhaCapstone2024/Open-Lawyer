255c67a43f543e460224555079d9d946f84237da
정재현 <chwogus0303@naver.com>
2024-11-22 19:46:42 +0900

    feat(springboot-app): 챗봇 API 호출 API 구현

---
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/chatbot/dto/response/UserQuestionResponseDto.java
@@ -0,0 +1,17 @@
+package com.inha.springbootapp.domain.chatbot.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserQuestionResponseDto {
+    private Double probability;
+    private String answer;
+}
