54d0c0e1f9e0d645f83bf8f41bed03ff408206c5
Jang Seung Beom <wkdtmf357@gmail.com>
2024-11-25 18:03:59 +0900

    feat(NotEnoughQustionCountExcepiton.java): 질문 차감 횟수 부족 Exception 클래스 구현

--- springboot-app/src/main/java/com/inha/springbootapp/domain/user/exception/NotEnoughQuestionCountException.java
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/user/exception/NotEnoughQuestionCountException.java
@@ -1,4 +1,19 @@
 package com.inha.springbootapp.domain.user.exception;
 
-public class NotEnoughQuestionCountException {
+public class NotEnoughQuestionCountException extends RuntimeException {
+    public NotEnoughQuestionCountException() {
+        super();
+    }
+
+    public NotEnoughQuestionCountException(String message) {
+        super(message);
+    }
+
+    public NotEnoughQuestionCountException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughQuestionCountException(Throwable cause) {
+        super(cause);
+    }
 }
