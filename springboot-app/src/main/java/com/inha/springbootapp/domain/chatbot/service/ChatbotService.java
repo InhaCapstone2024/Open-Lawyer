475d71540e779b34c4aed89113475bf9860035fd
Jang Seung Beom <wkdtmf357@gmail.com>
2024-11-25 18:15:20 +0900

    feat(ChatbotService.java): getPrediction, getAnswer 함수에 각 테이블 저장 및 질문 횟수 차감 로직 추가

--- springboot-app/src/main/java/com/inha/springbootapp/domain/chatbot/service/ChatbotService.java
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/chatbot/service/ChatbotService.java
@@ -1,8 +1,16 @@
 package com.inha.springbootapp.domain.chatbot.service;
 
 import com.inha.springbootapp.domain.chatbot.dto.request.ChatbotRequestDto;
+import com.inha.springbootapp.domain.chatbot.dto.request.UserQuestionRequestDto;
 import com.inha.springbootapp.domain.chatbot.dto.response.ChatbotResponseDto;
 import com.inha.springbootapp.domain.chatbot.dto.response.UserQuestionResponseDto;
+import com.inha.springbootapp.domain.chatbot.entity.ChatbotLog;
+import com.inha.springbootapp.domain.chatbot.entity.Question;
+import com.inha.springbootapp.domain.chatbot.repository.ChatbotLogRepository;
+import com.inha.springbootapp.domain.chatbot.repository.ChatbotRepository;
+import com.inha.springbootapp.domain.user.entity.User;
+import com.inha.springbootapp.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -12,34 +20,58 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class ChatbotService {
 
     @Value("${chatbot.base.url}")
     private String chatbotBaseUrl;
 
     private final RestTemplate restTemplate;
+    private final ChatbotRepository chatbotRepository;
+    private final UserService userService;
+    private final ChatbotLogRepository chatbotLogRepository;
 
-    ChatbotService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    public UserQuestionResponseDto getPrediction(ChatbotRequestDto request) {
+    public UserQuestionResponseDto getPrediction(UserQuestionRequestDto request, String accessToken) {
         String chatbotUrl = chatbotBaseUrl + "/api/chatbot/prediction";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<ChatbotRequestDto> requestEntity = new HttpEntity<>(request, headers);
-        return restTemplate.exchange(chatbotUrl, HttpMethod.POST, requestEntity, UserQuestionResponseDto.class).getBody();
+        HttpEntity<UserQuestionRequestDto> requestEntity = new HttpEntity<>(request, headers);
+        UserQuestionResponseDto userQuestionResponseDto = restTemplate.exchange(chatbotUrl, HttpMethod.POST, requestEntity, UserQuestionResponseDto.class).getBody();
+
+        User findUser = userService.getUserInfo(accessToken);
+        findUser.decreaseQuestionCount();
+
+        chatbotRepository.save(Question.builder()
+                .user(findUser)
+                .winPercentage(userQuestionResponseDto.getProbability())
+                .build());
+
+        return userQuestionResponseDto;
     }
 
-    public ChatbotResponseDto getAnswer(ChatbotRequestDto request) {
+    public ChatbotResponseDto getAnswer(ChatbotRequestDto request, String accessToken) {
         String chatbotUrl = chatbotBaseUrl + "/api/chatbot/question";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<ChatbotRequestDto> requestEntity = new HttpEntity<>(request, headers);
-        return restTemplate.exchange(chatbotUrl, HttpMethod.POST, requestEntity, ChatbotResponseDto.class).getBody();
+        ChatbotResponseDto chatbotResponseDto = restTemplate.exchange(chatbotUrl, HttpMethod.POST, requestEntity, ChatbotResponseDto.class).getBody();
+
+        User findUser = userService.getUserInfo(accessToken);
+        findUser.decreaseQuestionCount();
+
+        chatbotLogRepository.save(ChatbotLog.builder()
+                .userQuestion(request.getQuestion())
+                .answer(chatbotResponseDto.getAnswer())
+                .question(chatbotRepository.findById(request.getQuestionId())
+                        .orElseThrow(
+                                NullPointerException::new
+                        ))
+                .build());
+
+        return chatbotResponseDto;
     }
 }
