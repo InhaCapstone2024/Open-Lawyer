0dfd4a128bf295c343fdc7162054fef5551db5d1
정재현 <chwogus0303@naver.com>
2024-11-29 22:45:19 +0900

    test: 테스트

--- chatbot-app/entrypoint.sh
+++ chatbot-app/entrypoint.sh
@@ -2,12 +2,14 @@
 if [ ! -d /fastapi-app/venv ]; then \
     echo "[INFO] >> Installing dependencies, please wait..." && \
     python -m venv venv && \
-    source venv/bin/activate && \
+    . venv/bin/activate && \
+    pip install --upgrade pip && \
     pip install -r requirements.txt && \
     pip freeze > requirements.txt \
 ;else \
     echo "[INFO] >> Ready to launch server, checking new dependencies, please wait..." && \
-    source venv/bin/activate && \
+    . venv/bin/activate && \
+    pip install --upgrade pip && \
     pip install -r requirements.txt && \
     pip freeze > requirements.txt \
 ;fi
