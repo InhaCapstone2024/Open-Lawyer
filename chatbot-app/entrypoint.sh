64158bfe8bf010188d45477c64fe6f46825d5bca
정재현 <chwogus0303@naver.com>
2024-12-01 15:30:43 +0900

    refactor: 도커 파일 최적화

--- chatbot-app/entrypoint.sh
+++ chatbot-app/entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-if [ ! -d /fastapi-app/venv ]; then \
-    echo "[INFO] >> Installing dependencies, please wait..." && \
-    python -m venv venv && \
-    . venv/bin/activate && \
-    pip install --upgrade pip && \
-    pip install -r requirements.txt && \
-    pip freeze > requirements.txt \
-;else \
-    echo "[INFO] >> Ready to launch server, checking new dependencies, please wait..." && \
-    . venv/bin/activate && \
-    pip install --upgrade pip && \
-    pip install -r requirements.txt && \
-    pip freeze > requirements.txt \
-;fi
+
+# 가상 환경이 없다면 생성
+if [ ! -d ./venv ]; then
+    echo "[INFO] >> Creating virtual environment..."
+    python -m venv venv
+fi
+
+# 가상 환경 활성화 및 의존성 설치
+echo "[INFO] >> Installing dependencies..."
+. venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+pip freeze > requirements.txt
 
 # .env 파일에서 환경 변수 로드
 if [ -f .env ]; then
@@ -20,4 +19,5 @@ if [ -f .env ]; then
 fi
 
 # uvicorn 서버 실행
+echo "[INFO] >> Starting the server..."
 uvicorn main:app --host 0.0.0.0 --port ${SERVER_PORT} --reload
