1df193a66e5578657eda3d544cbcb46f8a7da2f2
Jang Seung Beom <wkdtmf357@gmail.com>
2024-12-05 23:57:39 +0900

    update(getNearbyLegalInstitutions.java): Post Method 변경 & Wrapper 클래스 반영

--- springboot-app/src/main/java/com/inha/springbootapp/domain/locate/controller/LegalInstitutionController.java
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/locate/controller/LegalInstitutionController.java
@@ -3,13 +3,16 @@ package com.inha.springbootapp.domain.locate.controller;
 import com.inha.springbootapp.domain.locate.dto.request.LegalInstitutionRequestDto;
 import com.inha.springbootapp.domain.locate.dto.response.LegalInstitutionResponseDto;
 import com.inha.springbootapp.domain.locate.service.LegalInstitutionService;
+import com.inha.springbootapp.domain.locate.wrapper.LegalInstitutionWrapper;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/legal-institution")
@@ -18,13 +21,13 @@ public class LegalInstitutionController {
     private final LegalInstitutionService legalInstitutionService;
 
     private final Dotenv dotenv = Dotenv.load();
+    String kakaoApiKey = dotenv.get("KAKAO_REST_API_KEY");
 
-    @GetMapping("/nearby")
-    public ResponseEntity<LegalInstitutionResponseDto> getNearbyLegalInstitutions(@RequestBody
-                                                                                      LegalInstitutionRequestDto legalInstitutionRequestDto){
-        LegalInstitutionResponseDto responseDto
-                = legalInstitutionService.getNearbyLegalInstitutions(legalInstitutionRequestDto, dotenv.get("KAKAO_REST_API_KEY"));
-
+    @PostMapping("/nearby")
+    public ResponseEntity<LegalInstitutionWrapper<List<LegalInstitutionResponseDto>>> getNearbyLegalInstitutions(
+            @RequestBody LegalInstitutionRequestDto legalInstitutionRequestDto){
+        LegalInstitutionWrapper<List<LegalInstitutionResponseDto>> responseDto
+                = legalInstitutionService.getNearbyLegalInstitutions(legalInstitutionRequestDto, kakaoApiKey);
         return ResponseEntity.ok(responseDto);
     }
 }
