000d6e8909c564a7be61b650479e27b9fe006de6
abyss-s <xyz@inha.edu>
2024-12-10 12:18:01 +0900

    🐛 Fix(WordModal.jsx): 법률 용어 모달 렌더링 버그 수정
    - tailwindcss z-index 제한(0-50)으로 명시적인 css로 변경
    - 반응형 모달 크기 수정
    - 스크롤 바를 description 섹션에만 적용

--- react-app/src/components/Word/WordModal.jsx
+++ react-app/src/components/Word/WordModal.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-
 const WordModal = ({ isOpen, term, onClose }) => {
   if (!isOpen || !term) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div
-        className="w-320 max-w-sm max-h-450 bg-white p-6 rounded-lg shadow-lg
-        overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100"
-      >
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+      style={{ zIndex: 1002 }} // tailwindcss z-index 제한으로 명시 필요
+    >
+      <div className="bg-white p-6 rounded-lg shadow-lg w-11/12 max-w-md sm:max-w-lg">
         <div className="flex justify-between items-center">
           <h2 className="text-lg font-bold">{term.termName}</h2>
           <button
@@ -18,8 +16,9 @@ const WordModal = ({ isOpen, term, onClose }) => {
             x
           </button>
         </div>
+        {/* 스크롤 가능한 용어 뜻 섹션 */}
         <div
-          className="mt-4 text-gray-700"
+          className="mt-4 text-gray-700 max-h-96 overflow-y-auto"
           dangerouslySetInnerHTML={{ __html: term.description }}
         />
       </div>
--- react-app/src/components/Word/WordTag.jsx
+++ react-app/src/components/Word/WordTag.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const WordTag = ({ term, onClick }) => {
   return (
     <div
-      className="border p-4 m-2 rounded-lg shadow-md flex justify-between cursor-pointer"
+      className="border p-4 m-2 rounded-lg shadow-md flex justify-between cursor-pointer bg-white"
       onClick={onClick}
     >
       <h2 className="text-base font-semibold">#{term.termName}</h2>
